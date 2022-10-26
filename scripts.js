f35f718316543f3d7e97f24315aa6560f03ca924
feriousdesteny <78489481+feriousdesteny@users.noreply.github.com>
2022-10-26 17:51:15 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,4 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-}
 const cards = document.querySelectorAll('.memory-card');
 
     let hasFlippedCard = false;
